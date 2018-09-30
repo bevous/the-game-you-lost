e49a39d586ecdd4aebcc182e702e0fc47799967a
bevous <43658573+bevous@users.noreply.github.com>
2018-09-30 11:16:52 -0500

    Add files via upload

---
+++ orbiter1/orbiter1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orbiter1
+{
+    class window
+    {
+        private 
+            int height=500;
+            int width = 500;
+
+        public
+            void OpenWindow()
+        {
+
+        }
+            void DrawElements()
+        {
+
+        }
+
+
+    }
+    class planet
+    {
+        
+        public bool Collision()
+        {
+            bool crassed = false;
+
+
+            return crassed;
+        }
+        public void Draw()
+        {
+
+        }
+
+    }
+    class player
+    {
+
+        private
+            double speed = 0.0;
+        double directionX = 0.0;
+        double directionY = 0.0;
+
+
+
+        public
+            bool Alive()
+        {
+            bool isAlive = true;
+
+
+            return isAlive;
+        }
+            bool Collision()
+        {
+            bool crassed = false;
+
+
+            return crassed;
+        }
+            void Draw()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //need a class for the player
+                //collision detection
+                //speed 
+                //direction
+            //class for planets
+                //collision detection
+                //needs to have gravity
+
+
+        }
+    }
+}
